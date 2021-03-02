113f409a5b55c50edad54648afcc3d723220446f
Leandran M <websitesa95@gmail.com>
2021-03-02 18:03:50 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,8 @@
 def position_taken?(board,index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
-
+ else
+   board[index] == "X" || board[index] == "O"
+   return true
   end
 end
