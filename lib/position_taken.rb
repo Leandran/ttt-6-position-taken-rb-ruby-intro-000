83ed3ace7eaa6f3c22747a7443463421d6c2d295
Leandran M <websitesa95@gmail.com>
2021-03-02 17:56:44 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,8 @@
 # code your #position_taken? method here!
 
 def position_taken?(board,index)
-  if index == " "
+  if index == " " || index == ""
     return false
-  elsif index == ""
-    return false
+
   end
 end
